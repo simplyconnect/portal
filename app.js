859fcc4ced460b08f071592c58817f67bf57ff66
simplyconnect <shahab.uddin@wesimcon.com>
2026-09-18 17:03:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
    CONFIG — Apps Script deploy karne ke baad /exec URL yahan daalein
 ===================================================================== */
 const CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzRESiiJcGoxCPdkeNOHMBns4FAq6LLYTxHm_TrvGb_n1lD_Ug_YPe7OlMh8rWyZOJk/exec'
 };
 
 /* =====================================================================
